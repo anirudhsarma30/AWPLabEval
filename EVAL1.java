8dae0082819983351a278c87b0106e8c07fab1b2
anirudhsarma30 <anirudhsarma30@gmail.com>
2020-01-27 18:42:58 +0530

    Update EVAL1.java

--- EVAL1.java
+++ EVAL1.java
@@ -94,5 +94,28 @@ class Customer implements Person
 class Location
 {
 	String name;
-	
+	int number;
+	Location(String NAME, int NUMBER)
+	{
+		name = NAME;
+		number = NUMBER;
+	}
+	void printLocation()
+	{
+		System.out.println((number+1)+". "+name);
+	}
+}
+
+public class CarRental
+{
+	static Location[] locations = {new Location("VNIT", 0),new Location("Ajni Station", 1), new Location("Eternity Mall", 2), new Location("Empress Mall", 3), new Location("INOX Jaswant Tuli", 4)}; 
+	static double[][] prices = {{0,5,4.6,6.5,6.3},{5,0,3.8,3.3,7.7},{4.6,3.8,0,2.4,6.2},{6.5,3.3,2.4,0,4.8},{6.3,7.7,6.2,4.8,0}};
+	static double getPrice(Location one, Location two)
+	{
+		return (25*prices[one.number][two.number]);
+	}
+	public static void main(String[] args)
+	{
+		//MAIN FUNCTION CODE.
+	}
 }
